fbe6062cbf2a354c66f3720bf7731a4ab0475eec
paulamp <paulapliegom@gmail.com>
2018-04-24 21:06:29 +0200

    entrega

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from cms import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+	url(r'^$', views.listar, name= 'Lista paginas'),
+	url(r'(.+)', views.mostrar, name ='pagina'),
     url(r'^admin/', include(admin.site.urls)),
 )
